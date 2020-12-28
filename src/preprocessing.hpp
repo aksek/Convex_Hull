332bb6703533fd0605fc192297b2be2d4b4eb2a4
aksek <kosekaniela@gmail.com>
2020-12-28 18:11:22 +0100

    Point class, voxelization

---
+++ src/preprocessing.hpp
@@ -0,0 +1,13 @@
+#ifndef PREPROCESSING_H
+#define PREPROCESSING_H
+
+#include<vector>
+#include"Point.hpp"
+
+class Preprocessing{
+public:
+    static void voxelize(std::vector<Point> &points, int d);
+};
+
+
+#endif //PREPROCESSING_H
