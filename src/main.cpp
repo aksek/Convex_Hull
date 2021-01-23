6389388acbd441dcc2babc4e110ca7da0b5edd66
kowusek <kowusek@gmail.com>
2021-01-23 23:45:02 +0100

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main() {
     data_generator g;
     data_converter c;
     int d = 1;
-    std::string cmd = "python3 ../src/visualize.py";
+    std::string cmd = "python3 ./src/visualize.py";
 
     g.generate_data(100, 100);
     std::vector<Point> points = c.load();
